721cb0fb46cf07177b4af609b80e6201d495f15d
MrHutsa <129012843+MrHutsa@users.noreply.github.com>
2023-04-22 20:56:15 -0400

    1.2.6.2

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,8 +1,96 @@
 
+
+document.getElementById("registro-form").addEventListener("submit", function(event) {
+    // Detener el envío del formulario para validación manual
+    event.preventDefault();
+  
+    // Obtener valores de los campos de entrada
+    var nombre = document.getElementById("nombre").value;
+    var usuario = document.getElementById("usuario").value;
+    var email = document.getElementById("email").value;
+    var telefono = document.getElementById("telefono").value;
+    var contraseña = document.getElementById("contraseña").value;
+    var confirmeContraseña = document.getElementById("confirmeContraseña").value;
+    var genero = document.querySelector('input[name="genero"]:checked');
+  
+    // Validar campos
+    var errores = [];
+    if (nombre.trim() === "") {
+      errores.push("Debe ingresar su nombre completo.");
+    }
+    if (usuario.trim() === "") {
+      errores.push("Debe ingresar un nombre de usuario.");
+    } else if (!/^[a-zA-Z0-9]{4,}$/.test(usuario)) {
+      errores.push("El nombre de usuario debe contener al menos 4 caracteres alfanuméricos.");
+    }
+    if (email.trim() === "") {
+      errores.push("Debe ingresar su correo electrónico.");
+    } else if (!/^\S+@\S+\.\S+$/.test(email)) {
+      errores.push("El correo electrónico ingresado no es válido.");
+    }
+    if (telefono.trim() === "") {
+      errores.push("Debe ingresar su número de teléfono.");
+    } else if (!/^[0-9]{3}[0-9]{3}[0-9]{3}$/.test(telefono)) {
+      errores.push("El número de teléfono ingresado no es válido. Use el formato 111-222-333.");
+    }
+    if (contraseña.trim() === "") {
+      errores.push("Debe ingresar una contraseña.");
+    }
+    if (confirmeContraseña.trim() === "") {
+      errores.push("Debe confirmar su contraseña.");
+    } else if (confirmeContraseña !== contraseña) {
+      errores.push("Las contraseñas no coinciden.");
+    }
+    if (!genero) {
+      errores.push("Debe seleccionar su género.");
+    }
+  
+    // Mostrar errores o enviar formulario
+    var erroresContainer = document.getElementById("errores");
+    if (errores.length > 0) {
+      erroresContainer.innerHTML = "";
+      for (var i = 0; i < errores.length; i++) {
+        var errorDiv = document.createElement("div");
+        errorDiv.className = "error";
+        errorDiv.textContent = errores[i];
+        erroresContainer.appendChild(errorDiv);
+      }
+    } else {
+      erroresContainer.innerHTML = "";
+      document.getElementById("registro-form").submit();
+    }
+    
+    // Obtener el botón de registro
+    const botonRegistro = document.querySelector('.button-box button');
+
+    // Agregar evento click al botón de registro
+    botonRegistro.addEventListener('click', function() {
+    // Redirigir al usuario al index.html
+    window.location.href = 'index.html';
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 <font-awesome-icon ico:icon="['fab', 'instagram']" bounce />
 */
-
+/*
 
 console.log("hola")
 // Nombre Completo
@@ -73,4 +161,4 @@ miTelefono.addEventListener('input', () => {
 miTelefono.addEventListener('invalid', () => {
     miTelefono.setCustomValidity('No es un número');
 });
-
+*/
